6a196f9c75f4bcf509ee88ea171185c5a8668d8f
helenegidion <103408420+helenegidion@users.noreply.github.com>
2022-09-22 15:05:06 +0200

    Update funktionen.js

--- funktionen.js
+++ funktionen.js
@@ -4,3 +4,14 @@
 
 
 let form = document.getElementById('formular');
+
+ function sendFormular(docForm,email,width,height){
+        if (docForm.exampleFormControlTextarea1.value.length == 0)
+        {
+          alert ("Sie haben keinen Text eingegeben!");
+          docForm.exampleFormControlTextarea1.focus();
+          return false;
+        }
+            
+
+    }
