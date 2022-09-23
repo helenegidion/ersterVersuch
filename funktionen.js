454ebe4ff38b5f9cbac3432169574bb95c3c74f2
helenegidion <103408420+helenegidion@users.noreply.github.com>
2022-09-23 15:31:57 +0200

    Update funktionen.js

--- funktionen.js
+++ funktionen.js
@@ -1,23 +1,24 @@
 document.addEventListener('DOMContentLoaded', function(){
     let anzahl = 0;
-    text = '';
+    let bestand = 0;
 
     document.getElementsByClassName("add").addEventListener('click', gedrueckt);
 
     function gedrueckt(){
         anzahl = anzahl + 1;
-        if (anzahl < 20){
-            text = 'Sie haben den Button ' + anzahl +'mal gedrückt!';
+        bestand = anzahl + 1;
+        try {
+            if(
+            bestand < 20)
+            anzahl++;
+        
         }
-        else if (anzahl < 15) {
-          text = 'Sie haben jetzt bereits ' + anzahl + 'mal geklickt.\n' +
-                 'Wollen Sie sich nicht einen Kaffee gönnen?';
-        }
-        else {
-          text = 'Jetzt haben Sie schon ' + anzahl + 'mal geklickt - mir wird langweilig.';
+        catch(Ecxeption) {
+          
         }
         document.querySelector('output').innerText = text;
       }
      
     }); 
+        
         
